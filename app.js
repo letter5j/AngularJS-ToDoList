530d27195676201a353e309ec99323dabb71e049
Log <seanletter5j@Gmail.com>
2016-03-18 04:07:16 +0800

    appHtml

---
+++ app.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Log on 2016/3/18.
+ */
+var app = angular.module("appToDoList", []);
+app.controller("appCtrl", function($scope) {
+    $scope.missionList = [];
+    $scope.addItem = function () {
+        $scope.errortext = "";
+        if (!$scope.addToDoItem) {
+            $scope.errortext = "Your input can not be empty.";
+            return;
+        }
+        if ($scope.missionList.indexOf($scope.addToDoItem) == -1) {
+            $scope.missionList.push($scope.addToDoItem);
+        } else {
+            $scope.errortext = "The task is already in your TodoList.";
+        }
+    };
+    $scope.removeItem = function (index_deletedItem) {
+        $scope.errortext = "";
+        $scope.missionList.splice(index_deletedItem, 1);
+    };
+});
