ce08994f23649795366e5420a21b98f01cd6673c
Log <seanletter5j@Gmail.com>
2016-03-19 17:33:44 +0800

    little update

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
 /**
  * Created by Log on 2016/3/18.
  */
-var app = angular.module("appToDoList", []);
-app.controller("appCtrl", function($scope) {
-    $scope.missionList = ["100", "200"];
+//var app = angular.module("appToDoList", []);
+var app = angular.module("appToDoList", ['ngCookies'] );
+app.controller("appCtrl", function($cookieStore, $scope) {
+    $scope.taskList = ["100", "200"];
     $scope.addItem = function () {
         $scope.errortext = "";
         if (!$scope.addToDoItem) {
             $scope.errortext = "Your input can not be empty.";
             return;
         }
-        if ($scope.missionList.indexOf($scope.addToDoItem) == -1) {
-            $scope.missionList.push($scope.addToDoItem);
+        if ($scope.taskList.indexOf($scope.addToDoItem) == -1) {
+            $scope.taskList.push($scope.addToDoItem);
         } else {
             $scope.errortext = "The task is already in your TodoList.";
         }
     };
     $scope.removeItem = function (index_deletedItem) {
         $scope.errortext = "";
-        $scope.missionList.splice(index_deletedItem, 1);
+        $scope.taskList.splice(index_deletedItem, 1);
     };
+
 });
