53baa2046bfce06461951ec0c009d3b2bd725c50
Log <seanletter5j@Gmail.com>
2016-03-19 22:19:57 +0800

    little change

--- app.js
+++ app.js
@@ -2,9 +2,10 @@
  * Created by Log on 2016/3/18.
  */
 //var app = angular.module("appToDoList", []);
-var app = angular.module("appToDoList", ['ngCookies'] );
-app.controller("appCtrl", function($cookieStore, $scope) {
-    $scope.taskList = ["100", "200"];
+var app = angular.module("appToDoList", ['ngCookies']);
+app.controller("appCtrl", function($scope, $cookies) {
+
+    $scope.taskList = [];
     $scope.addItem = function () {
         $scope.errortext = "";
         if (!$scope.addToDoItem) {
@@ -13,13 +14,29 @@ app.controller("appCtrl", function($cookieStore, $scope) {
         }
         if ($scope.taskList.indexOf($scope.addToDoItem) == -1) {
             $scope.taskList.push($scope.addToDoItem);
+            storeTasksInCookie();
+            
         } else {
             $scope.errortext = "The task is already in your TodoList.";
         }
     };
+    
+    var storeTasksInCookie = function(){
+        var now = new Date(),
+        exp = new Date(now.getFullYear()+1, now.getMonth(), now.getDate());
+        $cookies.put("taskList", $scope.taskList, {'expires': exp})
+       
+    };
+
+    var getTasksInCookie = function(){
+        return $cookies.get("taskList");
+    };
+    
+    
     $scope.removeItem = function (index_deletedItem) {
         $scope.errortext = "";
         $scope.taskList.splice(index_deletedItem, 1);
+        $scope.storeTasksInCookie();
     };
 
 });
